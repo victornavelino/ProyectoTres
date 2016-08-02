432676209c55d7436b5bf1af21ec9304939c83fc
hugo <hugo@debianHugo>
2016-08-02 18:37:51 -0300

    add entidades recertificacion y cuenta

---
+++ src/java/Entidades/Medico/Recertificacion.java
@@ -0,0 +1,171 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entidades.Medico;
+
+import Entidades.Pago.Cuenta;
+import Entidades.Usuario.Usuario;
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+
+/**
+ *
+ * @author hugo
+ */
+@Entity
+@Table(name = "recertificacion")
+public class Recertificacion implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @OneToOne
+    private Medico medico;
+    @OneToOne
+    private Cuenta cuenta;
+    @OneToOne
+    private Especializacion especializacion;
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date fechaRecertificacion;
+    private String nroActa;
+    private String nroResolucion;
+    private String libro;
+    private String folio;
+    @OneToOne
+    private Usuario usuario;
+    private String observaciones;
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date fechaVencimiento;
+    
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Medico getMedico() {
+        return medico;
+    }
+
+    public void setMedico(Medico medico) {
+        this.medico = medico;
+    }
+
+    public Cuenta getCuenta() {
+        return cuenta;
+    }
+
+    public void setCuenta(Cuenta cuenta) {
+        this.cuenta = cuenta;
+    }
+
+    public Especializacion getEspecializacion() {
+        return especializacion;
+    }
+
+    public void setEspecializacion(Especializacion especializacion) {
+        this.especializacion = especializacion;
+    }
+
+    public Date getFechaRecertificacion() {
+        return fechaRecertificacion;
+    }
+
+    public void setFechaRecertificacion(Date fechaRecertificacion) {
+        this.fechaRecertificacion = fechaRecertificacion;
+    }
+
+    public String getNroActa() {
+        return nroActa;
+    }
+
+    public void setNroActa(String nroActa) {
+        this.nroActa = nroActa;
+    }
+
+    public String getNroResolucion() {
+        return nroResolucion;
+    }
+
+    public void setNroResolucion(String nroResolucion) {
+        this.nroResolucion = nroResolucion;
+    }
+
+    public String getLibro() {
+        return libro;
+    }
+
+    public void setLibro(String libro) {
+        this.libro = libro;
+    }
+
+    public String getFolio() {
+        return folio;
+    }
+
+    public void setFolio(String folio) {
+        this.folio = folio;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getObservaciones() {
+        return observaciones;
+    }
+
+    public void setObservaciones(String observaciones) {
+        this.observaciones = observaciones;
+    }
+
+    public Date getFechaVencimiento() {
+        return fechaVencimiento;
+    }
+
+    public void setFechaVencimiento(Date fechaVencimiento) {
+        this.fechaVencimiento = fechaVencimiento;
+    }
+    
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Recertificacion)) {
+            return false;
+        }
+        Recertificacion other = (Recertificacion) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Entidades.Medico.Recertificacion[ id=" + id + " ]";
+    }
+    
+}
