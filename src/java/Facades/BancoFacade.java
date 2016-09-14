265a7feace8954330c07605e26d4166a1c7c853b
hugo <hugo@debianHugo>
2016-09-14 17:55:53 -0300

    modificacion lis especializxaconi

---
+++ src/java/Facades/BancoFacade.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Facades;
+
+import Entidades.Pago.Banco;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author hugo
+ */
+@Stateless
+public class BancoFacade extends AbstractFacade<Banco> {
+    @PersistenceContext(unitName = "ProyectoColegioPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public BancoFacade() {
+        super(Banco.class);
+    }
+    
+}
