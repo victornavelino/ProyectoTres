a339c20ad23c035bc6f0b5b014ad11f9490cfd08
hugo <hugo@debianHugo>
2016-09-15 15:53:13 -0300

    ingresos y cajas

---
+++ src/java/Facades/IngresoFacade.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Facades;
+
+import Entidades.Caja.Ingreso;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author hugo
+ */
+@Stateless
+public class IngresoFacade extends AbstractFacade<Ingreso> {
+    @PersistenceContext(unitName = "ProyectoColegioPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public IngresoFacade() {
+        super(Ingreso.class);
+    }
+    
+}
