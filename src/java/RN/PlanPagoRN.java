e83dbeebd036c4fe20183d3a941787af69714453
franco <franco@franco-notebook>
2016-10-12 20:34:10 -0300

    Al cargar el Plan de Pago se actualiza el cache de cuota plan de pago.

--- src/java/RN/PlanPagoRN.java
+++ src/java/RN/PlanPagoRN.java
@@ -4,7 +4,9 @@
  */
 package RN;
 
+import Entidades.Pago.CuotaPlanPago;
 import Entidades.Pago.PlanPago;
+import Facades.PlanPagoFacade;
 import java.util.List;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
@@ -21,27 +23,27 @@ public class PlanPagoRN implements PlanPagoRNLocal {
     @PersistenceContext(unitName = "ProyectoColegioPU")
     private EntityManager em;
     @EJB
-    private PlanPagoRNLocal ejbFacade;
+    private PlanPagoFacade ejbFacade;
 
     @Override
     public void create(PlanPago planPago) {
         //Agregar validaciones
         ejbFacade.create(planPago);
-        em.getEntityManagerFactory().getCache().evictAll();
+        em.getEntityManagerFactory().getCache().evict(CuotaPlanPago.class);
 
     }
 
     @Override
     public void edit(PlanPago planPago) {
         ejbFacade.edit(planPago);
-        em.getEntityManagerFactory().getCache().evictAll();
+        em.getEntityManagerFactory().getCache().evict(CuotaPlanPago.class);
 
     }
 
     @Override
     public void remove(PlanPago planPago) {
         ejbFacade.remove(planPago);
-        em.getEntityManagerFactory().getCache().evictAll();
+        em.getEntityManagerFactory().getCache().evict(CuotaPlanPago.class);
 
     }
 
